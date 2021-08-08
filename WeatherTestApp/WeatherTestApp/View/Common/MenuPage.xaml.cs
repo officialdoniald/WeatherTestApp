a9e40a3a1a9d22cf21ac6fa42171f3e2e3dec655
Bence Lénárt <bence960206@gmail.com>
2021-08-08 12:37:01 +0200

    Android tabbar and navigationbar coloring.

--- WeatherTestApp/WeatherTestApp/View/Common/MenuPage.xaml.cs
+++ WeatherTestApp/WeatherTestApp/View/Common/MenuPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.MVVM.ViewModel.Base;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using TabbedPage = Xamarin.Forms.TabbedPage;
 
 namespace WeatherTestApp.View.Common
 {
@@ -8,6 +11,9 @@ namespace WeatherTestApp.View.Common
         public MenuPage()
         {
             InitializeComponent();
+
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+            On<Android>().SetIsSwipePagingEnabled(false);
 
             NavigationPage.SetHasNavigationBar(this, false);
 
